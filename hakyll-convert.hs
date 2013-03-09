0df874713e954e9402d0135414e72524e55f14c2
Eric Kow <eric.kow@gmail.com>
2013-03-09 18:45:55 +0000

    Use filename closer to original URI (for cooler URIs).

--- hakyll-convert.hs
+++ hakyll-convert.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE ViewPatterns       #-}
 
 import           Control.Applicative
 import           Control.Arrow
+import           Control.Monad
 import qualified Data.ByteString        as B
 import           Data.Char
 import           Data.Function
@@ -18,12 +20,6 @@ import           Text.Atom.Feed
 import           Text.Atom.Feed.Export
 import           Text.Atom.Feed.Import
 import           Text.XML.Light
-
--- drafts vs posted?
--- comments vs posts
--- comments linked to posts
--- schemes
--- settings vs crap (can be done with id or category)
 
 data Config = Config
     { feed      :: FilePath
@@ -50,113 +46,136 @@ main = do
     mfeed <- readAtomFile (feed config)
     case mfeed of
         Nothing -> fail $ "Could not understand Atom feed: " ++ feed config
-        Just fd -> handleFeed config fd
+        Just fd -> processBloggerFeed config fd
 
-handleFeed config fd = do
-    createDirectoryIfMissing False (outputDir config)
-    mapM (savePost config) $ blocks
-  where
-    blocks = mapMaybe postAndComments
-           $ buckets getPost $ filter isInteresting
-           $ feedEntries fd
+processBloggerFeed config fd = do
+    mapM (savePost config) $ extractPosts (feedEntries fd)
 
--- | Given a list of entries in which exactly one of the entries
---   is a post and the rest are comments, return a tuple of the
---   post and its comemnts
-postAndComments (u,xs) =
-    case partition isPost xs of
-        ([p],cs) -> Just (p, cs)
-        ([],_)   -> Nothing -- spam comment(s) only?
-        _        -> error $ "Block with more than one post: " ++ u
-                          ++ concatMap displayEntry xs
-  where
-    isPost = any (isBloggerCategoryOfType "post") . entryCategories
+-- ---------------------------------------------------------------------
+-- From Blogger
+-- How Blogger represents posts, comments, etc in Atom
+-- ---------------------------------------------------------------------
 
--- | Save a post along with its comments as a mini atom feed
-savePost cfg (post, cmts) = do
-    putStrLn fname
-    writeFile (odir </> fname) $ ppElement $ xmlFeed feed
-  where
-    feed  = feed_ { feedEntries = post : cmts }
-    feed_ = nullFeed (entryId post) (entryTitle post) (fromMaybe "" $ entryPublished post)
-    odir  = outputDir cfg
-    fname = mkHakyllFilename post
+-- | A post and its comments
+data FullPost = FullPost
+    { fpPost     :: Entry
+    , fpComments :: [Entry]
+    , fpUri      :: String
+    }
 
--- | Return a string identifying the post that goes with the entry
---   If it's a comment, it should identify the post the comment is
---   for. If the entry is a post, it should identify the post itself.
-getPost e =
-    case (getLink isAlt, getLink isSelf) of
-        (Just l,  _)       -> postUrl l
-        (Nothing, Just l)  -> postUrl l
-        (Nothing, Nothing) -> error "don't know what with entry that has no alt/self"
+-- | An entry is assumed to be either a post, or a comment.
+--   If it's a post, it should be associated with the URI
+--   that visitors would use to read the post (on the old blog)
+--   If it's a comment, it should be the URI for the corresponding
+--   post.
+data BloggerEntry =
+    Post    { beUri_ :: String, beEntry :: Entry }
+  | Comment { beUri_ :: String, beEntry :: Entry }
+  | Orphan  { beEntry :: Entry }
+  deriving (Show)
+
+beUri :: BloggerEntry -> Maybe String
+beUri (Orphan _)    = Nothing
+beUri (Post u _)    = Just u
+beUri (Comment u _) = Just u
+
+-- | Warning: this silently ignores orphans, templates, settings
+extractPosts :: [Entry] -> [FullPost]
+extractPosts entries =
+    map toFullPost blocks
   where
-    getLink f = case filter f $ entryLinks e of
-                    []  -> Nothing
-                    [x] -> Just x
-                    xs  -> error $ "More than link of desired type for " ++ txtToString (entryTitle e)
+    toFullPost (uri, entries) = FullPost
+         { fpPost     = post
+         , fpComments = comments
+         , fpUri      = uri
+         }
+       where
+         post = case [ e | Post _ e <- entries ] of
+                    []  -> huh "Block of entries with no post?!"
+                    [p] -> p
+                    ps  -> huh "Block of entries with more than one post?!"
+         comments = [ e | Comment _ e <- entries ]
+         huh msg  = error . unlines $ msg : map (txtToString . entryTitle . beEntry) entries
+    blocks = [ (u,xs) | (Just u, xs) <- blocks_ ] -- drop orphans
+    blocks_ = buckets beUri
+            $ map identifyEntry
+            $ filter isInteresting entries
+
+-- | Tag an entry from the blogger feed as either being a post,
+--   a comment, or an "orphan" (a comment without an associated post)
+identifyEntry :: Entry -> BloggerEntry
+identifyEntry e =
+    if isPost e
+        then case getLink "self" `mplus` getLink "alternate" of
+                 Just l  -> Post (postUrl l) e
+                 Nothing -> entryError e oopsSelf
+        else case getLink "alternate" of
+                 Just l  -> Comment (postUrl l) e
+                 Nothing -> Orphan e
+  where
+    isPost  = any (isBloggerCategoryOfType "post") . entryCategories
     postUrl = takeWhile (/= '?') . linkHref
-    isAlt  l = linkRel l == Just (Right "alternate")
-    isSelf l = linkRel l == Just (Right "self")
+    getLink ty = case filter (isLink ty) $ entryLinks e of
+        []  -> Nothing
+        [x] -> Just x
+        xs  -> entryError e (oopsLink ty)
+    isLink ty l = linkRel l == Just (Right ty) && linkType l == Just "text/html"
+    oopsSelf    = "Was expecting blog posts to have a self link"
+    oopsLink ty = "Was expecting entries have at most one link of type " ++ ty
 
-
-
-displayEntry e = unlines
-    [ "-----------------------------------------------------------------"
-    , "#" ++ txtToString (entryTitle e)
-    , "Id: " ++ entryId e
-    , "Published:   " ++ maybe "N/A" id (entryPublished e)
-    , "In-Reply-To: " ++ show (entryInReplyTo e)
-    , "Categories:  " ++
-         (intercalate "," . map catTerm . filter (not . isBloggerCategory)
-          $ entryCategories e)
-    , "Hakyll name: " ++ mkHakyllFilename e
-    , ""
-    , if interesting
-         then take 2000 $ showContent (entryContent e)
-         else "-(boring)-"
-    ]
-  where
-    interesting = isInteresting e
-    showContent (Just (TextContent x)) = x
-    showContent (Just (HTMLContent x)) = x
-    showContent x = show x
-
-mkHakyllFilename e =
-    intercalate "-" pieces <.> "xml"
-  where
-    pieces = filter (not . null) $ [ dateStamp, mungedTitle ]
-    dateStamp   = maybe "date-unknown" (takeWhile (/= 'T')) $ entryPublished e
-    mungedTitle = reverse . dropWhile (== '_') . reverse
-                . intercalate "-"
-                . filter (not. null)
-                . map squish . words . txtToString $ entryTitle e
-    squish =
-        concatMap squishOne
-      where
-        squishOne x = if isAlphaNum x then [toLower x] else ['_']
-
+isBloggerCategory :: Category -> Bool
 isBloggerCategory = (== Just "http://schemas.google.com/g/2005#kind")
                   . catScheme
 
+isBloggerCategoryOfType :: String -- ^ \"comment\", \"post\", etc
+                        -> Category
+                        -> Bool
 isBloggerCategoryOfType ty c =
     isBloggerCategory c &&
     catTerm c == "http://schemas.google.com/blogger/2008/kind#" ++ ty
 
+-- | Contains actual meat (posts, comments; but not eg. templates)
+isInteresting :: Entry -> Bool
 isInteresting e =
     not $ any isBoring cats
   where
     isBoring c = any (\t -> isBloggerCategoryOfType t c) ["settings", "template"]
     cats       = entryCategories e
 
+-- ---------------------------------------------------------------------
+-- To Hakyll (sort of)
+-- Saving feed in bite-sized pieces
+-- ---------------------------------------------------------------------
+
+-- | Save a post along with its comments as a mini atom feed
+savePost :: Config -> FullPost -> IO ()
+savePost cfg post = do
+    putStrLn fname
+    createDirectoryIfMissing True (takeDirectory fname)
+    writeFile fname $ ppElement $ xmlFeed feed
+  where
+    mainPost = fpPost post
+    feed  = feed_ { feedEntries = fpPost post : fpComments post }
+    feed_ = nullFeed (entryId mainPost) (entryTitle mainPost) (fromMaybe "" $ entryPublished mainPost)
+    odir  = outputDir cfg
+    -- carelessly assumes we can treat URIs like filepaths
+    fname = odir </> dropExtensions (chopUri (fpUri post)) <.> "xml"
+    chopUri (dropPrefix "http://" -> ("",rest)) =
+        joinPath $ drop 1 $ splitPath rest -- drop the domain
+    chopUri u = error $
+        "We've wrongly assumed that blog post URIs start with http://, but we got: " ++ u
+
+-- ---------------------------------------------------------------------
+-- utilities
+-- ---------------------------------------------------------------------
+
 readAtomFile f = do
     parseAtomDoc <$> B.readFile f
   where
     parseAtomDoc x = elementFeed =<< parseXMLDoc x
 
--- ---------------------------------------------------------------------
---
--- ---------------------------------------------------------------------
+entryError e msg =
+    error $ msg ++ " [on entry " ++ entryId e ++ "]\n" ++ show e
 
 buckets :: Ord b => (a -> b) -> [a] -> [ (b,[a]) ]
 buckets f = map (first head . unzip)
@@ -164,22 +183,6 @@ buckets f = map (first head . unzip)
           . sortBy (compare `on` fst)
           . map (\x -> (f x, x))
 
-
-{-
--- | Complain if any of the specified arguments are missing
---   Use a somewhat compact output to make it a bit more readable
---
---   This is useful if you are abusing flags to get
---   non-positional-but-mandatory arguments
-dieOnMissingArgs :: String                     -- ^ program name
-                 -> config                     -- ^ arguments (eg. cmdargs struct)
-                 -> [ (config -> Bool, Text) ] -- ^ (isMissing, description)
-                 -> IO ()
-dieOnMissingArgs pname config missing_ =
-    unless (null missing) $ die . T.unlines $
-        [ "I need" <+> commas "and" missing
-        , "Try" <+> T.pack pname <+> " --help" -- FIXME: don't know how to print this automatically
-        ]
-  where
-    missing = [ n | (f,n) <- missing_, f config ]
--}
+dropPrefix :: Eq a => [a] -> [a] -> ([a],[a])
+dropPrefix (x:xs) (y:ys) | x == y    = dropPrefix xs ys
+dropPrefix left right = (left,right)
