97af326ac911a12f06778031130978468b9882ba
Alexander Batischev <eual.jp@gmail.com>
2020-09-04 00:14:32 +0300

    Add tests for OutputFormat.formatPath

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,12 @@
 import Test.Tasty (defaultMain, testGroup)
 
 import qualified Spec.Blogger as Blogger
+import qualified Spec.OutputFormat as OutputFormat
 import qualified Spec.Wordpress as Wordpress
 
 main :: IO ()
 main = defaultMain $ testGroup "Spec tests"
   [ Blogger.tests
   , Wordpress.tests
+  , OutputFormat.tests
   ]
